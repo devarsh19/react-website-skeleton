5bbb90db58e5b729fa3bdab55a57801041c3720e
devarsh19 <devarsh96patel@gmail.com>
2020-12-16 16:54:01 -0700

    initialize skeleton for making new components inside /src/components folder

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+    return (
+        <div>
+           Footer of the website will goes here 
+        </div>
+    );
+};
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div>
+           Header Navigation will display here 
+        </div>
+    );
+};
