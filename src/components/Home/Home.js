9145c42327b38e089f53523c6c307806d1cbfe30
devarsh19 <devarsh96patel@gmail.com>
2020-12-30 15:43:19 -0700

    created custom Carousel component to display on homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,75 +1,18 @@
 import React from 'react';
+import { CustomCarousel } from '../../sub-components/Carousel/CustomCarousel'
 import './Home.css'
 
 export const Home = () => {
     return (
         <div className="home">
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
-            Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
+            <div className="home__carousel">
+                <CustomCarousel />
+            </div>
+            <div className="home__container">
+                Welcome to cookbook website. We are currently making delicious food recipes, Once it ready we will show it to you here
+            </div>
+            
+             
              
         </div>
     );
